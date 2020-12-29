ba45a4435f97b0539dc0ab91a49a9e59421f641b
MikelTopKek <45463167+MikelTopKek@users.noreply.github.com>
2020-12-29 21:33:40 +0200

    Feature/bugfix args country (#4)
    * added Human and example
    
    * implemented Human class; added Storage usage example
    
    * done implement features in baseunit and squads and units
    
    * fix typo
    
    * [BUGFIX] update arguments namings for Country
    
    Co-authored-by: konzamir <konzamir@gmail.com>

--- countries.py
+++ countries.py
@@ -4,17 +4,17 @@ class Country:
                  tank_per_squad, human_per_squad):
         self.name = name
         self.index = index
-        self.humanCount = human_count
-        self.tankCount = tank_count
-        self.humanSquadsPerArmy = human_squads_per_army
-        self.tankSquadsPerArmy = tank_squads_per_army
-        self.tankPerSquad = tank_per_squad
-        self.humanPerSquad = human_per_squad
+        self.human_count = human_count
+        self.tank_count = tank_count
+        self.human_squads_per_army = human_squads_per_army
+        self.tank_squads_per_army = tank_squads_per_army
+        self.tank_per_squad = tank_per_squad
+        self.human_per_squad = human_per_squad
         self.list_of_humans = []
 
     def generate_units(self):
-        for i in range(1, self.humanCount):
+        for i in range(1, self.human_count):
             self.list_of_humans[i] = i
 
-        for i in range(1, self.tankCount):
+        for i in range(1, self.tank_count):
             self.list_of_humans[i] = i
