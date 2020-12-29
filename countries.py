9951bfc0da309855ac76083e78e8a9046d47ab44
Mikeltopkek <leniva9.panda@gmail.com>
2020-12-29 23:00:27 +0200

    update previous logic with countries and unit.py

--- countries.py
+++ countries.py
@@ -1,13 +1,13 @@
-class Country:
+from baseunit import BaseUnit
 
-    def __init__(self, name, index, human_count, tank_count, human_squads_per_army, tank_squads_per_army,
-                 tank_per_squad, human_per_squad):
+
+class Country(BaseUnit):
+
+    def __init__(self, name, index, human_count, tank_count, tank_per_squad, human_per_squad):
+        super().__init__(index)
         self.name = name
-        self.index = index
         self.human_count = human_count
         self.tank_count = tank_count
-        self.human_squads_per_army = human_squads_per_army
-        self.tank_squads_per_army = tank_squads_per_army
         self.tank_per_squad = tank_per_squad
         self.human_per_squad = human_per_squad
         self.list_of_humans = []
@@ -18,3 +18,6 @@ class Country:
 
         for i in range(1, self.tank_count):
             self.list_of_humans[i] = i
+
+    def attack(self, *args, **kwargs):
+        pass
