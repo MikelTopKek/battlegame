3ca8d86cb49119e0f4f9dce3b3e5a110076c1a42
MikelTopKek <45463167+MikelTopKek@users.noreply.github.com>
2020-12-30 00:13:36 +0200

    Feature/complete class war (#10)
    * complete class war
    
    * in storage removed unused import; create data.py; create a method for generating countries in war.py
    
    * fix lowercase on constant

---
+++ data.py
@@ -0,0 +1,18 @@
+COUNTRY_DATA = [
+    {
+        'index': 'ua',
+        'name': 'Ukraine',
+        'human_count': 112,
+        'tank_count': 28,
+        'tank_per_squad': 4,
+        'human_per_squad': 21
+    },
+    {
+        'index': 'kfc',
+        'name': 'KFC',
+        'human_count': 159,
+        'tank_count': 21,
+        'tank_per_squad': 5,
+        'human_per_squad': 33
+    }
+]
--- war.py
+++ war.py
@@ -1,13 +1,19 @@
+from countries import Country
+from data import COUNTRY_DATA
+from storage import Storage
+
+
 class War:
 
     def __init__(self, number_of_countries, number_of_turns):
         self.number_of_turns = number_of_countries
         self.number_of_countries = number_of_turns
-        self.number_of_countries = 0
-        self.number_of_turns = 0
 
     def start_battle(self):
         pass
 
     def generate(self):
-        pass
+        for i in range(self.number_of_countries):
+            country = Country(**COUNTRY_DATA[i])
+
+            Storage.countries.append(country)
