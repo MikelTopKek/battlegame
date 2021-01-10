ac6d474f31ef098a90dd9adb4f8ddaeff90257bd
konzamir <konzamir@gmail.com>
2021-01-10 16:34:45 +0200

    updated class War

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from war import War
 
 
 if __name__ == "__main__":
-    war = War(3, 1)
+    war = War()
     war.generate()
-    from storage import Storage
-    print(Storage.armies)
+    war.start_battle()
